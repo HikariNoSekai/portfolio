e34d950bf3f3e00fe00ac1aebafc8fba3ecd0ada
HikariNoSekai <obvilion08@gmail.com>
2018-03-07 12:16:02 +0200

    some changes

--- projects/luna/src/js/main.js
+++
@@ -1,8 +0,0 @@
-import "../components/header/nav.js"
-import "../components/about/about.js"
-import "../components/portfolio/ajaxload.js"
-import "../components/mobile/mobile.js"
-import "../components/social/social.js"
-import "../components/backToTop/scroll.js"
-import "../components/modal/modal_cl.js"
-import "../components/modal/modal.js"
